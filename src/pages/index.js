a20bba3f9a01ee211efd47350d46a47afcf58a35
tpiros <2042718+tpiros@users.noreply.github.com>
2023-05-15 11:28:41 +0800

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,14 @@
 import Image from 'next/image';
-
 export default function Home() {
   return (
     <div className="container">
-      <div className="left">
+      <div className="column">
         <h1>Next.js Image</h1>
         <Image src="/img/sample.jpg" alt="Sample" width={500} height={333} />
         <Image src="/img/sample2.jpg" alt="Sample" width={500} height={500} />
         <Image src="/img/sample3.jpg" alt="Sample" width={500} height={306} />
       </div>
-      <div className="right">
+      <div className="column">
         <h1>Cloudinary Image</h1>
         <img
           width="500"
